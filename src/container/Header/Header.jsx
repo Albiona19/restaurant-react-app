bff7e6eacf486c597e85f48b8ec2e931ccab288a
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2023-11-24 15:53:11 +0100

    header finished

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,16 @@ const Header = () => (
     <div className='app__wrapper_info'>
       <SubHeading title="Chase the new flavour" />
       <h1 className='app__header-h1'>The Key to Fine Dining</h1>
-
+      <p className="p__opensans" style={{margin: '2rem 0'}}>A restaurant is a place where people visit to eat and drink
+       the food being prepared on the premises and pays for the same.  The food is served at the table to have a comfortable visit for your meals. 
+       The restaurant offers a menu with various options for your meal, to choose from.</p>
+    
+      <button type='button' className='custom__button'>Explore Menu</button>
+    
+    
     </div>
     <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="" />
 
     </div>
   </div>
