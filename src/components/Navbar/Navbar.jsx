22f5ee04d20dc37c9acf9ab59c2f5775d7072955
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2023-11-24 14:46:00 +0100

    navbar hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
   <nav className='app__navbar'>
     <div className='app__navbar-logo'>
-
     <img src={images.gericht} alt="app logo" srcset="" />
     </div>
     <ul className='app__navbar-links'>
@@ -24,9 +25,13 @@ const Navbar = () => (
 
     </div>
     <div className="app__navbar-smallscren">
-      <GiHamburgerMenu  color='#fff' fontSize={27} onClick={()=>{}}/>
+      <GiHamburgerMenu  color='#fff' fontSize={27} onClick={()=> setToggleMenu(true)}/>
+      
+      
+      {toggleMenu &&(
+      
       <div className='app__navbar-smallscren_overlay flex__center slide-bottom'>
-       <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={()=>{}} />
+       <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={()=> setToggleMenu(false)} />
         <ul className='app__navbar-smallscren-links'>
         <li className='p__opensans'><a href='#home'>Home</a> </li>
         <li className='p__opensans'><a href='#about'>About</a> </li>
@@ -35,9 +40,10 @@ const Navbar = () => (
         <li className='p__opensans'><a href='#contact'>Contact</a> </li>
     </ul>
       </div>
+      )}
 
     </div>
   </nav>
 );
-
+  }
 export default Navbar;
