0d4a9dfc59e5095ee384343704208ebe57e07e44
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2023-11-27 13:04:36 +0100

    specialMneu started styled

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -15,13 +15,28 @@ const SpecialMenu = () => (
     <div className="app__specialMenu-menu_wine flex__center">
       <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
       <div className="app__specialMenu_menu_item">
-        {data.wines.map((wine, index)=>{
-          <p>{wine}</p>
-        })}
-
+        {data.wines.map((wine, index) => (
+          <p>{wine.title}</p>
+        ))}
       </div>
 
     </div>
+    <div className='app__specialMenu-menu_img'>
+      <img src={images.menu} alt="" />
+
+    </div>
+    <div className="app__specialMenu-menu_cocktails flex__center">
+      <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+      <div className="app__specialMenu_menu_item">
+        {data.cocktails.map((cocktails, index) => (
+          <p>{cocktails.title}</p>
+        ))}
+      </div>
+    </div>
+    </div>
+    <div style={{marginTop:'15px'}}>
+      <button type='button' className='custom__button'>View More</button>
+
     </div>
   </div>
 );
