263d330d80308f6c1bc742d6ba104fab70da1b28
Albiona19 <84641601+Albiona19@users.noreply.github.com>
2023-12-02 22:58:28 +0100

    intro section started

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Intro.css';
+import {BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import {meal} from '../../constants'
 
-const Intro = () => (
-  <div>
-    Intro
+const Intro = () => {
+  const [playVideo, setplayVideo] = React.useState(false)
+  const vidRef = React.useRef();
+
+  return(
+  <div className='app__video'>
+   <video src={meal}
+    type="video/mp4"
+     loop controls={false}
+       muted ref={vidRef}
+  />
+  <div className="app__video-overlay flex__center">
+    
   </div>
-);
+  </div>
+  )
+};
 
 export default Intro;
